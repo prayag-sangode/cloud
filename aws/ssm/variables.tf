2a005a209a045881ea4abd25f4686a1c174194e6
Prayag Sangode <67312119+prayag-sangode@users.noreply.github.com>
2025-12-06 01:35:18 +0530

    Create variables.tf

---
+++ aws/ssm/variables.tf
@@ -0,0 +1,20 @@
+variable "region" {
+  default = "us-east-1"
+  type    = string
+  description = "Region target to deploy resources"
+}
+
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+  type    = string
+  description = "CIDR block of the VPC"
+}
+
+variable "common_tags" {
+  type = map(string)
+  default = {
+    Environment = "dev"
+    Lab         = "ssm-private-ec2"
+  }
+  description = "Common tags for resources"
+}
